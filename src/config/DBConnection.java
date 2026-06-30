f0bb02623ee8cebc2ed3cde0d04d9c3e3245f2ec
Phan Hai Yen <phanyen06.ym@gmail.com>
2026-06-30 19:06:05 +0700

    Update project

--- src/config/DBConnection.java
+++ src/config/DBConnection.java
@@ -4,10 +4,32 @@
  */
 package config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 /**
  *
  * @author Admin
  */
+
+
 public class DBConnection {
+
+    private static final String URL =
+            "jdbc:sqlserver://localhost:1433;databaseName=QuanLyXeKhach;encrypt=true;trustServerCertificate=true";
+
+    private static final String USER = "sa";
+
+    private static final String PASSWORD = "123456";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Lỗi kết nối Database!");
+            e.printStackTrace();
+            return null;
+        }
+    }
     
 }
